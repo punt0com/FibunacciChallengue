a69bdd9e322653dfd96ee7816c963528fe2f049d
Andre Gomez <andremgm1@hotmail.com>
2020-02-11 09:45:21 -0600

    pushing the project

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Fibunacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int fibunacci = 0;
+            int sumatoria =0;
+            int num1 = 1;
+            int num2 = 1;
+            do
+            {
+                fibunacci = num1 + num2;
+                num2 = num1;
+                num1 = fibunacci;
+                if (fibunacci % 2 == 0)
+                {
+                    sumatoria += fibunacci;
+                }
+            } while (fibunacci < 4000000);
+
+            Console.WriteLine("La sumatoria es " + sumatoria);
+            Console.WriteLine("el ultimo numero Fibunacci es :" +fibunacci);
+        }
+
+    }
+}
